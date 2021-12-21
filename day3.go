9587f0f70126516f68bf0c42f28b4195386f96a9
Jakub Jandák <jakub.jandak@zasilkovna.cz>
2021-12-21 17:51:57 +0100

    day 3 simplification

--- day3.go
+++ day3.go
@@ -7,22 +7,23 @@ import (
 )
 
 func main() {
-	var gamma, epsilon string
-	var gammaInt, epsilonInt int64
+
 	testInput := inputter.GetLines("inputs/day3.test")
 	input := inputter.GetLines("inputs/day3.input")
 
-	gamma, epsilon = getRate(testInput)
-	gammaInt, _ = strconv.ParseInt(gamma, 2, 64)
-	epsilonInt, _ = strconv.ParseInt(epsilon, 2, 64)
-	fmt.Println("Part 1 test: ", gammaInt*epsilonInt)
-	gamma, epsilon = getRate(input)
-	gammaInt, _ = strconv.ParseInt(gamma, 2, 64)
-	epsilonInt, _ = strconv.ParseInt(epsilon, 2, 64)
-	fmt.Println("Part 1 test: ", gammaInt*epsilonInt)
+	fmt.Println("Part 1 test: ", getPart1Result(getRates(testInput)))
+	fmt.Println("Part 1 result: ", getPart1Result(getRates(input)))
+
 }
 
-func getRate(input []string) (string, string) {
+func getPart1Result(gamma string, epsilon string) (int64) {
+	var gammaInt, epsilonInt int64
+	gammaInt, _ = strconv.ParseInt(gamma, 2, 64)
+	epsilonInt, _ = strconv.ParseInt(epsilon, 2, 64)
+	return gammaInt * epsilonInt
+}
+
+func getRates(input []string) (string, string) {
 	gamma := ""
 	epsilon := ""
 	zeroCount := 0
